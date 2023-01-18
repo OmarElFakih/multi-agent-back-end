0b1a78a467498989b0e78eefe46ed01fed4d084b
OmarElFakih <omarelfakih97@gmail.com>
2023-01-18 16:15:55 -0400

    servidor y cliente basicos

---
+++ websocket_client.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const websocketClient = new WebSocket("ws://localhost:12345");
+
+    const messageContainer = document.querySelector("#message_container");
+    const messageInput = document.querySelector("[name=message_input]");
+    const sendMessageButton = document.querySelector("[name=send_message_button]");
+
+    console.log(messageInput);
+    console.log(sendMessageButton);
+    console.log(messageContainer);
+
+
+    websocketClient.onopen = function(){
+        sendMessageButton.onclick = function(){
+            websocketClient.send(messageInput.value);
+        }
+
+        websocketClient.onmessage = function(message){
+            const newMassage = document.createElement("div");
+            newMassage.innerHTML = message.data;
+            messageContainer.appendChild(newMassage);
+        }
+    };
+
+
+}, false);
