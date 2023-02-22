ddb4a886966093120e1fbbe2291893cdeff4509b
OmarElFakih <omarelfakih97@gmail.com>
2023-02-22 18:21:58 -0400

    mensajes a clientes especificos implementado

--- app/whatsapp_client.py
+++ app/whatsapp_client.py
@@ -17,7 +17,7 @@ class WhatsAppWrapper:
             "Authorization": f"Bearer {self.API_TOKEN}",
             "Content-Type": "application/json",
         }
-        self.API_URL = self.API_URL + self.NUMBER_ID
+        #self.API_URL = self.API_URL + self.NUMBER_ID
 
     def send_template_message(self, template_name, language_code, phone_number):
         payload = json.dumps({
@@ -38,7 +38,7 @@ class WhatsAppWrapper:
 
         return response.status_code
 
-    def send_message(self, body, phone_number):
+    def send_message(self, body, phone_number, business_number_id):
         payload = json.dumps({
             "messaging_product": "whatsapp",    
             "recipient_type": "individual",
@@ -50,9 +50,26 @@ class WhatsAppWrapper:
             }
         })
 
-        response = requests.request("POST", f"{self.API_URL}/messages", headers=self.headers, data=payload)
+        response = requests.request("POST", f"{self.API_URL}{business_number_id}/messages", headers=self.headers, data=payload)
 
         assert response.status_code == 200, "Error sending message"
 
         return response.status_code
+    
+    def request_data(self, body):
+        body_data = body["entry"][0]["changes"][0]["value"]
 
+        if "messages" in body_data:
+            data = {
+                "business_number": body_data["metadata"]["display_phone_number"],
+                "business_number_id": body_data["metadata"]["phone_number_id"],
+                "client_number": body_data["contacts"][0]["wa_id"],
+                "client_profile_name": body_data["contacts"][0]["profile"]["name"],
+                "message" : body_data["messages"][0]["text"]["body"],
+                "timestamp": body_data["messages"][0]["timestamp"]
+            }
+
+            return data
+        
+        else:
+            return "not a message"
--- application.py
+++ application.py
@@ -2,6 +2,7 @@ import os
 import aiohttp
 from app.whatsapp_client import WhatsAppWrapper
 from dotenv import load_dotenv
+import json
 
 from aiohttp import web
 #from aio.aplication import Application
@@ -10,7 +11,6 @@ routes = web.RouteTableDef()
 load_dotenv()
 all_clients = []
 wsClient = WhatsAppWrapper()
-nOfAgents = 0
 
 
 VERIFY_TOKEN = os.environ.get("WHATSAPP_VERIFY_TOKEN")
@@ -23,9 +23,10 @@ class Application(web.Application):
         return web.run_app(self, port=8000)
 
 
-async def send_all(message: str):
+async def send_all(message):
     for client in all_clients:
-        await client.send_str(message)
+        if client["business_number_id"] == message["business_number_id"]:
+            await client["connection"].send_str(json.dumps(message))
 
 @routes.get('/')
 async def hello(request):
@@ -54,13 +55,13 @@ async def verify_webhook(request):
 @routes.post('/webhook')
 async def webhook_notification(request):
     data = await request.json()
-    
-    if "messages" in data["entry"][0]["changes"][0]["value"]:
-        message_data = data["entry"][0]["changes"][0]["value"]["messages"][0]
-        print(f'{message_data["text"]["body"]}')
-        await send_all(f'{message_data["from"]}: {message_data["text"]["body"]}')
+    message_data = wsClient.request_data(data)
 
-    print(data)
+    if(message_data != "not a message"):
+        print(message_data)
+        await send_all(message_data)
+
+    #print(data)
     
     return web.Response(text="success")
 
@@ -70,29 +71,35 @@ async def websocket_handler(request):
 
     ws = web.WebSocketResponse()
     await ws.prepare(request)
-    global nOfAgents
-
-    id = nOfAgents
-
-    nOfAgents += 1
     
+    client = {
+        "connection": ws,
+        "business_number_id": ""
+    }
 
-    all_clients.append(ws)
+    all_clients.append(client)
+    print("client connected")
 
     async for msg in ws:
         if msg.type == aiohttp.WSMsgType.TEXT:
-            #if msg.data == 'close':
-            #   await ws.close()
-            #else:
-                #await ws.send_str(f'{id}: {msg.data}')
-                await send_all(f'Agent{id}: {msg.data}')
-                wsClient.send_message(msg.data, "584123722632")
+                data = json.loads(msg.data)
+                
+                #await send_all()
+                print(data)
+                
+                if data["type"] == "connection":
+                    print("connection")
+                    client["business_number_id"] = data["business_number_id"]
+
+                if data["type"] == "message":
+                    wsClient.send_message(data["message"], data["client_number"], client["business_number_id"])
+
         elif msg.type == aiohttp.WSMsgType.ERROR:
             print('ws connection closed with exception %s' %
                   ws.exception())
 
     print('websocket connection closed')
-    all_clients.remove(ws)
+    all_clients.remove(client)
     return ws
 
 application = Application()
